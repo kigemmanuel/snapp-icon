b0d41d74bcec8bf73635db90a44846079f17ac61
kigemmanuel <kigemmanuel.mail@gmail.com>
2025-10-24 08:18:15 +0100

    latest updates, Added types

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'snapp-icons' {
+  export const Fontawesome: any;
+  // Future exports:
+  // export const MaterialIcon: any;
+  // export const Heroicons: any;
+  // export const Lucide: any;
+}
